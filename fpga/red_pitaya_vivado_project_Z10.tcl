92fa1aef6b8dd69999e4dcee8086a6c19f637ea8
Amar Dadel <76798367+amar98-hash@users.noreply.github.com>
2021-09-28 08:21:10 -0700

    Update red_pitaya_vivado_project_Z10.tcl

--- fpga/red_pitaya_vivado_project_Z10.tcl
+++ fpga/red_pitaya_vivado_project_Z10.tcl
@@ -14,11 +14,11 @@ cd C:/Users/Amar/Documents/Xilinx_workspace/RedPitaya-master/fpga/$prj_name
 # define paths
 ################################################################################
 
-set path_brd C:\Users\Amar\Documents\Xilinx_workspace\RedPitaya-master/fpga/brd
-set path_rtl C:\Users\Amar\Documents\Xilinx_workspace\RedPitaya-master/fpga/rtl
-set path_ip  C:\Users\Amar\Documents\Xilinx_workspace\RedPitaya-master/fpga/ip
+set path_brd C:/Users/Amar/Documents/Xilinx_workspace/RedPitaya-master/fpga/brd
+set path_rtl C:/Users/Amar/Documents/Xilinx_workspace/RedPitaya-master/fpga/rtl
+set path_ip  C:/Users/Amar/Documents/Xilinx_workspace/RedPitaya-master/fpga/ip
 set path_bd  project/redpitaya.srcs/sources_1/bd/system/hdl
-set path_sdc C:\Users\Amar\Documents\Xilinx_workspace\RedPitaya-master/fpga/sdc
+set path_sdc C:/Users/Amar/Documents/Xilinx_workspace/RedPitaya-master/fpga/sdc
 set path_sdc_prj sdc
 
 ################################################################################
