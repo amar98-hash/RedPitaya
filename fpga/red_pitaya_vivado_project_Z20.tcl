2d78f4288070278c78fe897b2c30cd887bfc9511
Amar Dadel <76798367+amar98-hash@users.noreply.github.com>
2021-09-30 10:54:36 -0700

    Update red_pitaya_vivado_project_Z20.tcl

--- fpga/red_pitaya_vivado_project_Z20.tcl
+++ fpga/red_pitaya_vivado_project_Z20.tcl
@@ -14,12 +14,14 @@ cd prj/$prj_name
 # define paths
 ################################################################################
 
-set path_brd ../../brd
-set path_rtl rtl
-set path_ip  ip
+
+set path_brd C:/Users/Amar/Documents/Xilinx_workspace/RedPitaya-master/fpga/brd
+set path_rtl C:/Users/Amar/Documents/Xilinx_workspace/RedPitaya-master/fpga/rtl
+set path_ip  C:/Users/Amar/Documents/Xilinx_workspace/RedPitaya-master/fpga/prj/v0.94/ip
 set path_bd  project/redpitaya.srcs/sources_1/bd/system/hdl
-set path_sdc ../../sdc
+set path_sdc C:/Users/Amar/Documents/Xilinx_workspace/RedPitaya-master/fpga/sdc
 set path_sdc_prj sdc
+
 
 ################################################################################
 # list board files
@@ -53,7 +55,7 @@ generate_target all [get_files    system.bd]
 # 3. constraints
 ################################################################################
 
-add_files                         ../../$path_rtl
+add_files                         $path_rtl
 add_files                         $path_rtl
 add_files                         $path_bd
 
